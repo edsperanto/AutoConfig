2b6fe9cfa0e5038eb8023b4d0c2c679c0aa2c034
Edward Gao <edsperanto@users.noreply.github.com>
2019-02-16 14:05:10 -0800

    created formatting file

---
+++ color.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BLACK_FG=`tput setaf 0`
+RED_FG=`tput setaf 1`
+GREEN_FG=`tput setaf 2`
+YELLOW_FG=`tput setaf 3`
+BLUE_FG=`tput setaf 4`
+MAGENTA_FG=`tput setaf 5`
+CYAN_FG=`tput setaf 6`
+WHITE_FG=`tput setaf 7`
+BLACK_BG=`tput setab 0`
+WHITE_BG=`tput setab 7`
+RESET=`tput sgr0`
+BOLD=`tput bold`
+UNDERLINE=`tput smul`
+NO_UNDERLINE=`tput rmul`
