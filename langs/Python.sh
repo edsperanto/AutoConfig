c8d7a438f1f83776929a2278fd4aa1dd1db4f899
Jack <josephj4@seattleu.edu>
2019-02-16 22:36:45 -0800

    added Python.sh version selection box

--- langs/Python.sh
+++ langs/Python.sh
@@ -1,3 +1,14 @@
 #!bin/bash
 
-echo "Python sh!"
+selectVersion() {
+    if (whiptail --title "Python" --yesno \
+--yes-button "Python 2" --no-button "Python 3" \
+"Choose your desired Python version:" 10 60) then
+		echo "Python 2"
+	else
+		echo "Python 3"
+	fi
+}
+
+selectVersion
+
