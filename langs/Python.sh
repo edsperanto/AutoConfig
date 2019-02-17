dfd5617252ea7050ff95c8e580215858007f7909
Jack <josephj4@seattleu.edu>
2019-02-17 12:51:52 -0800

    python options update config file when language is disabled

--- langs/Python.sh
+++ langs/Python.sh
@@ -3,34 +3,53 @@ source helpers.sh
 
 configFile=$1
 
-# yesno whiptail to select python version
-#selectVersion() {
-#    if (whiptail --title "Python" --yesno \
-#--yes-button "Python 2" --no-button "Python 3" \
-#"Choose your desired Python version:" 10 60) then
-#        $(change "language.python.version.3" "false" "$configFile")
-#       	$(change "language.python.version.2" "true" "$configFile")
-#
-#		echo $(valueOf "language.python.version.2" "$configFile")
-#	else
-#        change "language.python.version.3" "true" "$configFile"
-#        change "language.python.version.2" "false" "$configFile"
-#	fi
-#}
+# gets current preferences from config file
+loadPreferences() {
+	if [ $(valueOf "language.python.use" "$configFile") ]; then
+		prefs[0]="ON"
+	fi
+	
+	if [ $(valueOf "language.python.version.3" "$configFile") ]; then
+		prefs[1]="ON"
+	fi
+
+	if [ $(valueOf "language.python.package.pandas" "$configFile") ]; then
+		prefs[2]="ON"
+	fi
+
+	if [ $(valueOf "language.python.package.numpy" "$configFile") ]; then
+		prefs[3]="ON"
+	fi
+}
 
 
 # library selection whiptail checklist
 configureOptions() {
+	loadPreferences
+
 	libs=$(whiptail --title "Python" --checklist \
 "Configure Python options:" \
 --nocancel --ok-button "Done" 15 60 4 \
-"Python" "(Uncheck to disable the language)" ON \
-"Python 3" "(Uncheck for Python 2)" ON \
-"Requests" "HTTP for Humans" OFF \
-"NumPy" "Computing with Python" OFF 3<&1 1<&2 2<&3)
+"Python" "(Uncheck to disable the language)" ${prefs[0]} \
+"Python 3" "(Uncheck for Python 2)" ${prefs[1]} \
+"Pandas" "Data structures & analysis" ${prefs[2]} \
+"NumPy" "Computing with Python" ${prefs[3]} 3<&1 1<&2 2<&3)
 	# TODO just echoes selections for now
-	echo $libs
+	if [[ $libs == *"\"Python\""* ]]; then
+		$(change "language.python.use" "true" "$configFile")
+		
+		
+	else
+		$(change "language.python.use" "false" "$configFile")
+		$(change "language.python.version.3" "false" "$configFile")
+		$(change "language.python.version.2" "false" "$configFile")
+		$(change "language.python.package.numpy" "false" "$configFile")
+		$(change "language.python.package.pandas" "false" "$configFile")
+
+	fi
+
 }
 
+prefs=("OFF" "OFF" "OFF" "OFF")
 configureOptions
 
