2368bf9c85b3eff820c21a005d37765d52e14658
Jack <josephj4@seattleu.edu>
2019-02-16 23:44:04 -0800

    added Python.sh library selection checklist

--- langs/Python.sh
+++ langs/Python.sh
@@ -1,14 +1,28 @@
 #!bin/bash
 
+# yesno whiptail to select python version
 selectVersion() {
     if (whiptail --title "Python" --yesno \
 --yes-button "Python 2" --no-button "Python 3" \
 "Choose your desired Python version:" 10 60) then
+		# TODO just echoes choice for now
 		echo "Python 2"
 	else
 		echo "Python 3"
 	fi
 }
 
+
+# library selection whiptail checklist
+selectLibraries() {
+	libs=$(whiptail --title "Python" --checklist \
+"Select libraries to install:" 15 60 2 \
+"Requests" "HTTP for Humans" OFF \
+"NumPy" "Computing with Python" OFF 3<&1 1<&2 2<&3)
+	# TODO just echoes selections for now
+	echo $libs
+}
+
 selectVersion
+selectLibraries
 
